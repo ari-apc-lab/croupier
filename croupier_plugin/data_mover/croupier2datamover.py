c1d43498b0003b9a42f04905b3a43a82c6a9c5a4
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-04-24 14:21:06 +0200

    Fixed commented code

--- croupier_plugin/data_mover/croupier2datamover.py
+++ croupier_plugin/data_mover/croupier2datamover.py
@@ -24,25 +24,25 @@ def move_data(data_mover_options, type_transfer):
     grid_usercert = data_mover_options['grid_usercert']
     grid_cert_passwd = data_mover_options['grid_certpass']
     cloud_folder = data_mover_options['cloud_folder']
-    hpc_folder = data_mover_options['local_folder']
+    hpc_folder = data_mover_options['hpc_folder']
 
-    # try:
-    #     # gridproxy is not initialized if userkey=="" or usercert==""
-    #     mydatamover = DataMover(create_ws, hpc_user_id, cloud_user, hpc_user_ssh_credentials,
-    #                             ws_name, ws_lifetime, grid_userkey, grid_usercert, grid_cert_passwd)
-    #     if type_transfer == "download":
-    #         source = "ATOSFR"
-    #         destination = "HLRS"
-    #         source_input = cloud_folder
-    #         dest_output = hpc_folder
-    #     else:
-    #         source = "HLRS"
-    #         destination = "ATOSFR"
-    #         source_input = hpc_folder
-    #         dest_output = cloud_folder
-    #
-    #     size_bytes = mydatamover.run_transference(source, destination, source_input, dest_output)
-    # except Exception as exp:
-    #     print(traceback.format_exc())
-    #     ctx.logger.error(
-    #         'Something happend when trying to invoke DataMover: ' + exp.message)
+    try:
+        # gridproxy is not initialized if userkey=="" or usercert==""
+        mydatamover = DataMover(create_ws, hpc_user_id, cloud_user, hpc_user_ssh_credentials,
+                                ws_name, ws_lifetime, grid_userkey, grid_usercert, grid_cert_passwd)
+        if type_transfer == "download":
+            source = "ATOSFR"
+            destination = "HLRS"
+            source_input = cloud_folder
+            dest_output = hpc_folder
+        else:
+            source = "HLRS"
+            destination = "ATOSFR"
+            source_input = hpc_folder
+            dest_output = cloud_folder
+
+        size_bytes = mydatamover.run_transference(source, destination, source_input, dest_output)
+    except Exception as exp:
+        print(traceback.format_exc())
+        ctx.logger.error(
+            'Something happend when trying to invoke DataMover: ' + exp.message)
