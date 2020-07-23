120c2ce814af7d467d5b6b9a299026e03200b0ac
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-07-23 12:24:56 +0200

    Fix: Configuring Accounting
    Signed-off-by: Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>

--- croupier_plugin/accounting_client/accounting_client.py
+++ croupier_plugin/accounting_client/accounting_client.py
@@ -31,6 +31,7 @@ import os
 
 import requests
 from requests import HTTPError
+from cloudify import ctx
 
 from model.base import AccountingBase
 from model.user import User
@@ -80,12 +81,16 @@ class AccountingClient:
         # Configure Accounting endpoint from configuration file
         config = ConfigParser.RawConfigParser()
         config_file = str(os.path.dirname(os.path.realpath(__file__))) + '/../Croupier.cfg'
+        ctx.logger.info('Reading Accounting configuration from file {file}'.format(
+            file=config_file))
         config.read(config_file)
-        accounting_endpoint = config.get('Accounting', 'endpoint')
-        if accounting_endpoint is not None:
-            self.endpoint = accounting_endpoint
-        else:
-            self.endpoint = endpoint
+        self.endpoint = endpoint
+        try:
+            accounting_endpoint = config.get('Accounting', 'endpoint')
+            if accounting_endpoint is not None:
+                self.endpoint = accounting_endpoint
+        except ConfigParser.NoSectionError:
+            pass
 
     # Users API client
     def get_users(self):
