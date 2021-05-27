de5dbd046683938c6714f00696fd495953e30d6a
Marangio <paolo.marangio@atos.net>
2021-05-27 10:56:58 +0200

    Add fix change naming of log files of HPC jobs

--- croupier_plugin/workflows.py
+++ croupier_plugin/workflows.py
@@ -72,8 +72,10 @@ class JobGraphInstance(object):
 
             # build job name
             instance_components = instance.id.split('_')
-            self.name = runtime_properties["job_prefix"] +\
-                instance_components[-1]
+            self.name = instance_components[0] + instance_components[1]
+
+            #self.name = runtime_properties["job_prefix"] +\
+            #    instance_components[-1]
         else:
             self._status = 'NONE'
             self.name = instance.id
