e7e1589cdaf5a9b8f736cc240015eb47dbd0219a
Jesús Ramos Rivas <jesus.ramos.external@atos.net>
2021-07-23 17:38:14 +0200

    Fixed jobid registration

--- croupier_plugin/workflows.py
+++ croupier_plugin/workflows.py
@@ -182,8 +182,9 @@ class JobGraphInstance(object):
     @staticmethod
     def register_jobid(pseudo_deployment_id, name, jobid, logger):
         if pseudo_deployment_id not in DB_JOBID:
-            DB_JOBID[pseudo_deployment_id] = {}
-        DB_JOBID[pseudo_deployment_id][name] = jobid
+            DB_JOBID[pseudo_deployment_id] = {name: jobid}
+        else:
+            DB_JOBID[pseudo_deployment_id][name] = jobid
         logger.debug("In register_jobid method, just registered jobid "+jobid+" DB_JOBID:\n"
                          + str(DB_JOBID[pseudo_deployment_id]))
 
