a70c4436893822efd0818072ac836c4c84a6d8ed
Cloud User <centos@centos-testing.novalocal>
2021-07-29 11:33:40 +0000

    added a test blueprint and release script

--- croupier_plugin/tests/integration/workflow_tests.py
+++ croupier_plugin/tests/integration/workflow_tests.py
@@ -131,6 +131,27 @@ class TestPlugin(unittest.TestCase):
             logging.warning('[WARNING] Login could not be tested')
 
     @workflow_test(
+        os.path.join('blueprints', 'blueprint_grapevine_test.yaml'),
+        copy_plugin_yaml=True,
+        resources_to_copy=[(os.path.join('blueprints', 'inputs_def.yaml'), './')],
+        inputs='set_inputs')
+    def test_grapevine(self, cfy_local):
+        """ Single BATCH Job Blueprint """
+        cfy_local.execute('install', task_retries=0)
+        cfy_local.execute('run_jobs', task_retries=0)
+        cfy_local.execute('uninstall', task_retries=0)
+
+        # extract single node instance
+        instance = cfy_local.storage.get_node_instances()[0]
+
+        # due to a cfy bug sometimes login keyword is not ready in the tests
+        if 'login' in instance.runtime_properties:
+            # assert runtime properties is properly set in node instance
+            self.assertEqual(instance.runtime_properties['login'], True)
+        else:
+            logging.warning('[WARNING] Login could not be tested')
+
+    @workflow_test(
         os.path.join('blueprints', 'blueprint_single.yaml'),
         copy_plugin_yaml=True,
         resources_to_copy=[(os.path.join('blueprints', 'inputs_def.yaml'), './')],
