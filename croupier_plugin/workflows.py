22f531e7c017d88bbec96286b2f09260e0fd5454
Jesús Ramos Rivas <jesus.ramos.external@atos.net>
2021-09-03 10:02:44 +0200

    Removed printing runtime_properties on logs

--- croupier_plugin/workflows.py
+++ croupier_plugin/workflows.py
@@ -56,7 +56,6 @@ class JobGraphInstance(object):
             self._status = 'WAITING'
             # Get runtime properties
             runtime_properties = instance._node_instance.runtime_properties
-            ctx.logger.info("runtime_properties:" + str(runtime_properties))
             self.simulate = runtime_properties["simulate"]
             self.host = runtime_properties["credentials"]["host"]
             self.workdir = runtime_properties['workdir']
