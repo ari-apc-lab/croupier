abe38c49d93783b942e01284b27cabbe24ad851b
Jesús Ramos Rivas <jesus.ramos.external@atos.net>
2021-11-02 11:27:26 +0100

    Changed ITAINNOVA test to make use of the Data node (URL)

--- croupier_plugin/tests/integration/blueprints/itainnova/scripts/bootstrap.sh
+++ croupier_plugin/tests/integration/blueprints/itainnova/scripts/bootstrap.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-export shared=${STORE}/ATOS-WORKSHOP
-export environment_file=${shared}/environment.yaml
-export script_file=${shared}/Codigo/Train_hard_eat_healthy_sleep_well_and_repeat-Your_brain-$1.py
+export shared=${GRAPEVINE}/ATOS-WORKSHOP
+export script_file=${shared}/Codigo/test-$1.py
 export PHENOLOGY_MODEL_LOCATION=$2
 export OUTPUT_FILE_LOCATION=$3
 
 cat << EOF > execute.sh
 #!/bin/bash
+module load miniconda3
 export PHENOLOGY_MODEL_LOCATION=${PHENOLOGY_MODEL_LOCATION}
 export OUTPUT_FILE_LOCATION=${OUTPUT_FILE_LOCATION}
-conda env create --file ${environment_file}
-source activate grapevine
+conda activate ${shared}/grapevine
 python ${script_file}
 EOF
