7938c5a9002ae736ec5ae39e859e57a26b4eb320
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2022-01-19 17:21:58 +0100

    feat: submission of PyCOMPSs jobs

--- croupier_plugin/infrastructure_interfaces/pycompss.py
+++ croupier_plugin/infrastructure_interfaces/pycompss.py
@@ -24,107 +24,132 @@ license information in the project root.
 pycompss.py
 """
 from croupier_plugin.infrastructure_interfaces.infrastructure_interface import InfrastructureInterface
-from croupier_plugin.infrastructure_interfaces.slurm import start_time_tostr, get_job_metrics, _parse_states
 from croupier_plugin.ssh import SshClient
+from past.builtins import basestring
 
 
 class Pycompss(InfrastructureInterface):
+    def _parse_job_settings(self, job_id, job_settings, script=False, timezone=None):
+        # Not required
+        pass
+
     def _get_jobid(self, output):
         return output.split(' ')[-1].strip()
 
-    def _parse_job_settings(
-            self,
-            job_id,
-            job_settings,
-            script=False,
-            timezone=None):
-        """
-            Generates Manager specific job submission script/call
-            Invoked by InfrastructureInterface at:
-                _build_script: to create a Manager specific job submission script content
-                    This is only invoked when job submission script content needs to be generated
-                    from job specification in workflow. Other alternatives supported by Infrastructure interface
-                    includes:
-                        a) using a remote script,
-                        b) an script content given with the job specification in workflow
-                        c) an script file given as resource within the workflow zip
-                _build_job_submission_call: to create a Manager specific job submission call
-                    job_settings include the name of the script to create for "script" key
-                    and additional arguments to pass to the job submission script
-                    In caller, job submission script will be enclosed by prefixes and sufixes
-                    given in job specification
-            @type job_id: string
-            @param job_id: name of the job
-            @type job_settings: dictionary
-            @param job_settings: dictionary with the job options
-            @script
-            @timezone
-            @return dict with two keys:
-             'data' parsed data with its parameters, and
-             'error' if an error arise.
-            """
-        pass
-
     def _build_job_cancellation_call(self, name, job_settings):
         """
-        Creates the job cancellation command
+        Creates the PyCOMPSs job cancellation command
         """
-        return "scancel --name " + name
-
-    def _get_envar(
-            self,
-            envar,
-            default):
-        if envar == 'SCALE_INDEX':
-            return '$SLURM_ARRAY_TASK_ID'
-        elif envar == 'SCALE_COUNT':
-            return '$SLURM_ARRAY_TASK_COUNT'
-        else:
-            return default
+        pass
 
     def get_states(self, ssh_config, job_names):
         """
-        Uses Monitoring-enabled Manager for getting job state and accounting audits
-        Slurm by default (for PYCOMPSs), given the job submission id
+        Uses PyCOMPSs Manager for getting job state and accounting audits
         """
-        monitor_start_time_str = start_time_tostr(self.monitor_start_time)
-        call = "sacct -n -o JobName,State -X -P --name=" + ','.join(job_names) + " -S " + monitor_start_time_str
-        client = SshClient(ssh_config)
-        output, exit_code = client.execute_shell_command(
-            call,
-            workdir=self.workdir,
-            wait_result=True)
-        states = {}
-        if exit_code == 0:
-            states = _parse_states(output, self.logger)
-        else:
-            self.logger.error("Failed to get job states: " + output)
-
-        # Get job execution audits for monitoring metrics
-        audits = {}
-        for name in job_names:
-            if name in states:
-                if states[name] != 'PENDING':
-                    audits[name] = get_job_metrics(name, client, self.workdir, monitor_start_time_str, self.logger)
-            else:
-                self.logger.warning("Could not parse the state of job: " + name + "Parsed dict:" + str(states))
-        client.close_connection()
-        return states, audits
+        pass
 
     def delete_reservation(self, ssh_client, reservation_id, deletion_path):
-        """
-        Manage the deletion of a reservation (if supported with PYCOMPSs, using
-        SLURM by default, given the reservation id
-        """
-        call = 'sudo {0} {1}'.format(deletion_path, reservation_id)
-        output, exit_code = ssh_client.execute_shell_command(
-            call,
-            wait_result=True)
-        if exit_code == 0:
-            return True
-        else:
-            return False
+        # Not required
+        pass
 
     def _add_audit(self, job_id, job_settings, script=False, ssh_client=None):
         # Not supported
         pass
+
+    def submit_job(self,
+                   ssh_client,
+                   name,
+                   job_settings,
+                   is_singularity,
+                   context=None,
+                   environment=None,
+                   timezone=None):
+        """
+        Sends a job to the HPC
+
+        @type ssh_client: SshClient
+        @param ssh_client: ssh client connected to an HPC login node
+        @type name: string
+        @param name: name of the job
+        @type job_settings: dictionary
+        @param job_settings: dictionary with the job options
+        @rtype string
+        @param context: cloudify context
+        @type context: cloudify.ctx
+        @param environment: Dictionary containing context env vars
+        @type environment: dictionary of strings
+        @param timezone: Timezone of the HPC the job is being submitted to
+        @rtype string
+        @return Slurm's job id sent. None if an error arise.
+        """
+        if not SshClient.check_ssh_client(ssh_client, self.logger):
+            self.logger.error('check_ssh_client failed')
+            return False
+
+        # build the command to submit the job
+        submission_command = self._build_job_submission_call(name, job_settings, ssh_client, timezone=timezone)
+
+        if 'error' in submission_command:
+            self.logger.error("Couldn't build the command to send the PyCOMPSs job: " + submission_command['error'])
+            return False
+
+        # submit the job
+        command = submission_command['command']
+
+        output, exit_code = ssh_client.execute_shell_command(command, env=environment, workdir=self.workdir,
+                                                             wait_result=True)
+        if exit_code != 0:
+            self.logger.error("Job submission '" + command + "' exited with code " + str(exit_code) + ":\n" + output)
+            return False
+
+        return self._get_jobid(output)
+
+    def _build_job_submission_call(self, name, job_settings, ssh_client, timezone):
+        """
+        Generates submission command line as a string
+        submission call:
+        export COMPSS_PYTHON_VERSION=3
+        module load COMPSs/2.10
+        pycompss job submit -e [ENV_VAR...] -app APP_NAME [COMPSS_ARGS] APP_FILE [APP_ARGS]
+
+        @type name: string
+        @param name: name of the job
+        @type job_settings: dictionary
+        @param job_settings: dictionary with the job options
+        @rtype dict
+        @return dict with possible keys:
+         'command' string to call pycompss with its parameters
+          'error' if an error arise.
+        """
+        response = {}
+        # check input information correctness
+        if not isinstance(job_settings, dict) or \
+                not isinstance(name, basestring):
+            return {'error': "Incorrect inputs"}
+        if 'app_name' not in job_settings:
+            return {'error': "app_name not provided"}
+        if 'app_file' not in job_settings:
+            return {'error': "app_file not provided"}
+
+        # Build PyCOMPSs submission command from job_settings
+        _command = 'export COMPSS_PYTHON_VERSION=3; module load COMPSs/2.10; pycompss job submit'
+
+        if 'env' in job_settings:
+            for env_entry in job_settings['env']:
+                for key in env_entry:
+                    _command += ' -e ' + key + '=' + str(env_entry[key])
+
+        _command += ' -app ' + job_settings['app_name']
+
+        if 'compss_args' in job_settings:
+            for key in job_settings['compss_args']:
+                _command += ' --' + key + '=' + str(job_settings['compss_args'][key])
+
+        _command += ' ' + job_settings['app_file']
+
+        if 'app_args' in job_settings:
+            for arg in job_settings['app_args']:
+                _command += ' ' + str(arg)
+
+        response['command'] = _command
+        return response
