ad1a81630a17de8c7f27beb0e05f270229c90eb3
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2022-03-04 11:01:34 +0100

    fix: Cloudify execution of deployment

--- croupier_plugin/tests/integration/blueprints/pycompss-drug-synergies-deploy/scripts/deploy.sh
+++ croupier_plugin/tests/integration/blueprints/pycompss-drug-synergies-deploy/scripts/deploy.sh
@@ -2,7 +2,6 @@
 set -e
 
 # read arguments
-
 if [ "$#" -lt 6 ]; then
     echo "Illegal number of parameters.
     Usage: deploy -u|--user <hpc_user> -p|--password <hpc_password -k|--private_key <hcp_pkey> -h|--host <hpc_host> [-t|--token <github_token>].
@@ -66,21 +65,22 @@ function cleanup {
   echo "Deleted temp working directory $WORK_DIR"
 }
 # register the cleanup function to be called on the EXIT signal
-# trap cleanup EXIT
+trap cleanup EXIT
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORK_DIR=$(mktemp -d -p "$DIR")
 # check if tmp dir was created
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-  echo "Could not create temp dir where to download Covid19 app"
+  echo "Could not create temp dir where to download Drug Synergies app"
   exit 1
 fi
 
-echo "Deploying Covid19 into temp working directory $WORK_DIR"
+echo "Deploying Drug Synergies into temp working directory $WORK_DIR"
 cd "$WORK_DIR" || exit
 
-# Get Covid19 app from Github in temp folder
+# Get Drug Synergies app from Github in temp folder
+echo "Downloading Drug Synergies app into temp working directory $WORK_DIR"
 if [ -z "$github_token" ]; then
   wget https://github.com/PerMedCoE/drug-synergies-workflow/archive/refs/heads/main.zip
 else
@@ -88,11 +88,13 @@ else
 fi
 
 #Unzip repo
+echo "Unzipping Drug Synergies app"
 unzip main.zip drug-synergies-workflow-main/Resources/data/*
 unzip main.zip drug-synergies-workflow-main/Workflow/PyCOMPSs/src/*
 tar xvzf drug-synergies-workflow-main/Resources/data/data_celllines.tar.gz
 
-#Rsync transfer Covid19 app and data to target HPC using user user's credentials and ssh
+#Rsync transfer Drug Synergies app and data to target HPC using user user's credentials and ssh
+echo "Transferring Drug Synergies app to $hpc_host:permedcoe_apps/drug-synergies"
 if [ -n "$hpc_pkey" ]; then
   rsync -ratlz -e "ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i $hpc_pkey" drug-synergies-workflow-main "$hpc_user"@"$hpc_host":permedcoe_apps/drug-synergies
 fi
